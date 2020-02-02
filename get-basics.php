06bab8f3e3cbb7d22b7a0c6da2dfcb6ee1517a0c
Eccenux <eccenux+git@gmail.com>
2020-02-02 13:06:09 +0100

    refactor

--- get-basics.php
+++ get-basics.php
@@ -5,7 +5,8 @@
 date_default_timezone_set('Europe/Warsaw');
 
 require_once './inc/Ticks.php';
-$ticks = new cTicks();
+require_once './inc/Parser.php';
+//$ticks = new cTicks();
 
 //
 // Settings
@@ -20,51 +21,10 @@ $infoStep = 1000;
 //
 // Parse/Dump
 //
-$totalRecords = 0;
-$stepsCount = 1;
-$ticks->pf_insTick('parse');
-$ticks->pf_insTick('parse-step-'.$stepsCount);
-foreach (glob("{$baseInputPath}/**/*") as $filename) {
-	//echo "$filename size " . filesize($filename) . "\n";
-	$json = json_decode (file_get_contents($filename));
-
-	// id
-	if (empty($json->id)) {
-		echo "\n[ERROR] Id jest puste!";
-		var_export($json);
-		echo "\n";
-		continue;
-	}
-
-	$totalRecords++;
-	if ($totalRecords % $infoStep === 0) {
-		echo "\n[INFO] Records read: $totalRecords.";
-		$ticks->pf_endTick('parse-step-'.$stepsCount);
-		$stepsCount++;
-		$ticks->pf_insTick('parse-step-'.$stepsCount);
-		//break; // quick test
-	}
-	
-	// adres
-	if (!empty($json->adres)) {
-		echo "\n[WARNING] ({$json->id}) Niepusty adres!";
-		var_export($json);
-		die();
-	} else {
-		//echo "\n[INFO] ({$json->id}) Pusty adres.";
-	}
-}
-$ticks->pf_endTick('parse-step-'.$stepsCount);
-$ticks->pf_endTick('parse');
+$parser = new Parser();
+$parser->parse($baseInputPath);
 
 //
 // Info/summary
 //
-echo "\n";
-echo "\n[INFO] Total: $totalRecords.";
-
-$arrTicks = $ticks->pf_getDurations();
-echo "\n[INFO] Timers [s]:";
-foreach ($arrTicks as $strTickName=>$intDurtation) {
-	echo sprintf("\n%s: %.4f", $strTickName, $intDurtation);
-}
+$parser->dumpInfo();
---
+++ inc/Parser.php
@@ -0,0 +1,92 @@
+<?php
+require_once './inc/Ticks.php';
+
+class Parser
+{
+	private $ticks;
+	private $infoStep = 1000;	//!< Progress info
+	private $totalRecords = 0;	//!< Records read
+
+	/**
+	 * Init.
+	 * @param integer $infoStep Progress info step.
+	 */
+	public function __construct($infoStep = null)
+	{
+		$this->ticks = new cTicks();
+		if (!empty($infoStep)) {
+			$this->infoStep = $infoStep;
+		}
+	}
+
+	/**
+	 * Parser loop.
+	 *
+	 * Assumes all files are JSON files.
+	 * 
+	 * @param string $baseInputPath Base path (subdirectories will be searched for files).
+	 * @param callback $recordCallback
+	 *	Callback for getting data (gets json object as 1st param).
+	 *	Return false to break the loop.
+	 */
+	public function parse($baseInputPath, $recordCallback = null) {
+		//
+		// Parse/Dump
+		//
+		$this->totalRecords = 0;
+		$stepsCount = 1;
+		$this->ticks->pf_insTick('parse');
+		$this->ticks->pf_insTick('parse-step-'.$stepsCount);
+		foreach (glob("{$baseInputPath}/**/*") as $filename) {
+			//echo "$filename size " . filesize($filename) . "\n";
+			$json = json_decode (file_get_contents($filename));
+
+			// id
+			if (empty($json->id)) {
+				echo "\n[ERROR] Id jest puste!";
+				var_export($json);
+				echo "\n";
+				continue;
+			}
+
+			$this->totalRecords++;
+			if ($this->totalRecords % $this->infoStep === 0) {
+				echo "\n[INFO] Records read: $this->totalRecords.";
+				$this->ticks->pf_endTick('parse-step-'.$stepsCount);
+				$stepsCount++;
+				$this->ticks->pf_insTick('parse-step-'.$stepsCount);
+				//break; // quick test
+			}
+
+			// adres
+			if (!empty($json->adres)) {
+				echo "\n[WARNING] ({$json->id}) Niepusty adres!";
+				var_export($json);
+				die();
+			} else {
+				//echo "\n[INFO] ({$json->id}) Pusty adres.";
+			}
+
+			if (is_callable($recordCallback)) {
+				$result = $recordCallback($json);
+				if ($result === false) {
+					break;
+				}
+			}
+		}
+		$this->ticks->pf_endTick('parse-step-'.$stepsCount);
+		$this->ticks->pf_endTick('parse');
+	}
+
+	public function dumpInfo() {
+		echo "\n";
+		echo "\n[INFO] Total: $this->totalRecords.";
+
+		$arrTicks = $this->ticks->pf_getDurations();
+		echo "\n[INFO] Timers [s]:";
+		foreach ($arrTicks as $strTickName=>$intDurtation) {
+			echo sprintf("\n%s: %.4f", $strTickName, $intDurtation);
+		}
+	}
+
+}
